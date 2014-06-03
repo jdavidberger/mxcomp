86b70d5a7d9c612a587bdd5bfeaf9310556a1fd7
jberger <none@none>
2014-06-03 16:28:03 -0600

    test stuff

--- src/vsostream.cc
+++ src/vsostream.cc
@@ -9,13 +9,27 @@
 #pragma warning(default: 4265)
 
 namespace mxcomp {
-     static std::mutex vsmutex;
+     
      template <class CharT, class TraitsT = std::char_traits<CharT> >
      struct vsstreambuf : public std::basic_stringbuf<CharT, TraitsT> {
+          std::mutex vsmutex;
           virtual ~vsstreambuf() { sync(); }
+          virtual int_type underflow() {
+               std::lock_guard<std::mutex> g(vsmutex);
+               return std::basic_stringbuf<CharT, TraitsT>::underflow();
+          }
+          virtual int_type overflow(int_type c = traits_type::eof()) {
+               std::lock_guard<std::mutex> g(vsmutex);
+               return std::basic_stringbuf<CharT, TraitsT>::overflow(c);
+          }
+          virtual int_type pbackfail(int_type c = traits_type::eof()) {
+               std::lock_guard<std::mutex> g(vsmutex);
+               return std::basic_stringbuf<CharT, TraitsT>::pbackfail(c);
+          }
      protected:
           int sync()
           {
+               
                std::lock_guard<std::mutex> g(vsmutex);
                output_debug_string(str().c_str());
                str(std::basic_string<CharT>());
